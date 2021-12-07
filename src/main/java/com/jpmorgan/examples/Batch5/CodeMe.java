58c807704d2ccb55671bda74198738872f4ac976
J3-Aquacool <jagdish.java2@gmail.com>
2021-12-07 17:09:59 +0530

    Added another set of code

---
+++ src/main/java/com/jpmorgan/examples/Batch5/CodeMe.java
@@ -0,0 +1,13 @@
+package com.jpmorgan.examples.Batch5;
+
+public class CodeMe {
+String username;
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+String password;
+
+}
